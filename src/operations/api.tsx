2e2236d96c82061b2bae5e811a42a107ad33a9e6
avigupta250 <avinashkumarnav52@gmail.com>
2024-12-08 20:05:02 +0530

    api/v1

--- src/operations/api.tsx
+++ src/operations/api.tsx
@@ -3,8 +3,8 @@
 
 export const BASE_URL = import.meta.env.VITE_BASE_URL
 export const endPoints={
-    SIGN_UP:`${BASE_URL}/signup`,
-    SIGN_IN:`${BASE_URL}/signin`,
+    SIGN_UP:`${BASE_URL}/api/v1/signup`,
+    SIGN_IN:`${BASE_URL}/api/v1/signin`,
     CONTENT:`${BASE_URL}/content`,
     SHARE:`${BASE_URL}/brain/share`,
     LINK:`${BASE_URL}/brain/:shareLink`,
