2c524f66d2a672586c1f8611873a3c9ea0e176b9
avigupta250 <avinashkumarnav52@gmail.com>
2024-12-08 18:10:44 +0530

    removed unsued imports

--- src/components/shadcn/BentoGrid.tsx
+++ src/components/shadcn/BentoGrid.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { cn } from "../../lib/utils";
-import React from "react";
+
 import { BentoGrid, BentoGridItem } from "../ui/bento-grid";
 import {
   IconBoxAlignRightFilled,
